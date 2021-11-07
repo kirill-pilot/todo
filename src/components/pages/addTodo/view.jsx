95aad8583f8189e07a79c50eac362737c46c04db
Kirill Lakotko <kirill.pilot@gmail.com>
2021-11-07 19:42:49 +0300

    add dispatch addtodo action in AddPage

--- src/components/pages/addTodo/view.jsx
+++ src/components/pages/addTodo/view.jsx
@@ -1,18 +1,25 @@
 import { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Layout } from '../../layouts/default';
+import { addTodo } from '../../../redux/actionCreators/todos';
 
 function AddPage() {
   const [inputValue, setInputValue] = useState('');
+  const dispatch = useDispatch();
 
   const handelChangeInput = useCallback((event) => {
     setInputValue(event.target.value);
   }, []);
+  const handleClick = useCallback(() => {
+    dispatch(addTodo(inputValue));
+    setInputValue('');
+  }, [dispatch, inputValue]);
 
   return (
     <div>
       <Layout>
         <input type="text" value={inputValue} onChange={handelChangeInput} />
-        <button >Add</button>
+        <button onClick={handleClick}>Add</button>
       </Layout>
     </div>
   );
